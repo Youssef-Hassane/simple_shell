836fe83bdbcfdba58dba58e2af7b66dbfb4b2044
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-12-13 22:38:05 +0200

    added Includes file

---
+++ Includes.c
@@ -0,0 +1,81 @@
+#include "main.h"
+/* get_The_Input_From_The_User - Reads a line from the standard input */
+#include GET_THE_INPUT_FROM_THE_USER
+/* is_It_A_Terminal - Checks if a file descriptor is a terminal */
+#include IS_IT_A_TERMINAL
+/* read_Line - Reads a line from the standard input */
+#include READ_LINE
+/* string_Tokenization - A function that breaks a string into tokens */
+#include STRING_TOKENIZATION
+/* write_Function - A function that writes a string to a file */
+#include WRITE_FUNCTION
+/* remove_Comments - Removes comments from a string */
+#include REMOVE_COMMENTS
+/* handle_Quotes_And_Comments - A function that handles quotes and comments */
+#include HANDLE_QUOTES_AND_COMMENTS
+/* free_And_Assign_Null - Frees allocated memory and returns NULL */
+#include FREE_AND_ASSIGN_NULL
+/* free_And_Return_Null - Frees allocated memory and returns NULL */
+#include FREE_AND_RETURN_NULL
+/* free_The_Two_Dimensional_Array - Free the two dimensional array */
+#include FREE_THE_TWO_DIMENSIONAL_ARRAY
+/* get_Builtin_Index - Get the index of a */
+/* command in the built-in commands array */
+#include GET_BUILTIN_INDEX
+/* process_Builtin_Command - Process built-in shell commands */
+#include PROCESS_BUILTIN_COMMAND
+/* check_If_The_Command_Is_Builtin - Checks if the command */
+/* is a built-in command */
+#include CHECK_IF_THE_COMMAND_IS_BUILTIN
+/* handle_Exit_Input - Handles the exit command */
+#include HANDLE_EXIT_INPUT
+/* terminate_Shell - Terminates the shell */
+#include TERMINATE_SHELL
+/* write_Error_Message - Writes an error message to the standard output */
+#include WRITE_ERROR_MESSAGE
+/* display_Environment - Display the current environment */
+#include DISPLAY_ENVIRONMENT
+/* convert_Integer_To_String - Convert an integer to a string */
+#include CONVERT_INTEGER_TO_STRING
+/* the_Given_Number_Is_Positive_Or_Not - Checks if a string */
+/* is a positive number */
+#include THE_GIVEN_INPUT_IS_POSITIVE_OR_NOT
+/* parse_Command_Into_Tokens - Parses a command into tokens */
+#include PARSE_COMMAND_INTO_TOKENS
+/* iterate_and_count_tokens - Counts the number of tokens */
+#include ITERATE_AND_COUNT_TOKENS
+/* cleanup_After_Execution - Cleanup after the */
+/* execution of the child process */
+#include CLEANUP_AFTER_EXECUTION
+/* display_Error_Message - Display an error message */
+#include DISPLAY_ERROR_MESSAGE
+/* execute_Child_Process - Execute the child process using execve */
+#include EXECUTE_CHILD_PROCESS
+/* execute_The_Given_Command - Execute the command in a child process */
+#include EXECUTE_THE_GIVEN_COMMAND
+/* get_Exit_Status - Get the exit status from the waitpid status */
+#include GET_EXIT_STATUS
+/* handle_Execution_Error - Handle execution error and cleanup */
+#include HANDLE_EXECUTION_ERROR
+/* set_Environment_Variable - Set or modify an environment variable */
+#include SET_ENVIRONMENT_VARIABLE
+/* handle_Setenv_Input - Handle the setenv built-in command */
+#include HANDLE_SETENV_INPUT
+/* handle_unsetenv_input - Handle the unsetenv built-in command */
+#include HANDLE_UNSETENV_INPUT
+/* unset_Environment_Variable - Unset or remove an environment variable */
+#include UNSET_ENVIRONMENT_VARIABLE
+/* get_The_Environment_Variable - Get the value of */
+/* a specified environment variable */
+#include GET_THE_ENVIRONMENT_VARIABLE
+/* get_The_Path_Of_Command - Gets the path of a command */
+#include GET_THE_PATH_OF_COMMAND
+/* is_It_Absolute_Path - Checks if the command is an absolute path */
+#include IS_IT_ABSOLUTE_PATH
+/* searchAndRetrieveEnvironmentVariableValue - Search for a */
+/* specified environment variable and retrieve its value */
+#include SEARCH_ENV_VAR_VALUE_PATH
+/* search_In_Directories - Search for the command in directories */
+#include SEARCH_IN_DIRECTORIES
+/* execute_Command - Executes the command */
+#include EXECUTE_COMMAND
